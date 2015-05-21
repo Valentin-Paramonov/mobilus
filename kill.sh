4a643ea0d4657a693758e2fff9d47b324c24d302
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-05-21 22:19:19 +0300

    Add run and kill scripts

---
+++ kill.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+[ ! -e .pid ] && echo "Not running?" && exit 1
+kill $(cat .pid) && rm .pid
+
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+[ -e .pid ] && echo "Already running?" && exit 1
+node server.js >> server.log &
+echo $! > .pid
+
