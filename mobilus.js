267c16f4e7ad1c0f5f5cf1a2b9b064c3c13b275f
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-05-23 15:38:59 +0300

    Rename applciation to "mobilus"

---
+++ mobilus.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+var mobilus = express();
+
+var HOME = __dirname,
+    PORT = 4848;
+
+mobilus.use(bodyParser.json());
+mobilus.set('view engine', 'jade');
+mobilus.set('views', HOME + '/views');
+mobilus.locals.pretty = true;
+
+mobilus.use('/test-data', require('./routes/test-data'));
+
+mobilus.get('/', function(request, response) {
+    response.send('Data received!');
+});
+
+mobilus.post('/', function(request, response) {
+    console.log(request.body);
+    response.send(request.body);
+});
+
+mobilus.get('/log', function(request, response) {
+    response.render('log');
+});
+
+mobilus.listen(PORT, function() {
+    console.log('Listening on port ' + PORT);
+});
