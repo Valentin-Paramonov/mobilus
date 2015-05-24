414e323779ff56e71d39c6ea8abf57baf02074e8
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-05-25 02:18:25 +0300

    Fetch with interval

--- web/lib/mobile-stats.js
+++ web/lib/mobile-stats.js
@@ -1,11 +1,21 @@
 (function() {
     var app = angular.module('mobileStats', []);
-    app.controller('statsController', function($scope, $http) {
-        $http.get('http://52.28.111.52:4848/statistics')
-            .success(function(stats) {
-                $scope.stats = stats;
-            }).error(function(err) {
-                console.error(err);
-            });
+    app.controller('statsController', function($scope, $interval, $http) {
+        var fetchStatistics = function() {
+            $http.get('http://52.28.111.52:4848/statistics')
+                .success(function(stats) {
+                    $scope.stats = stats;
+                }).error(function(err) {
+                    console.error(err);
+                });
+        };
+        fetchStatistics();
+        var fetchPromise = $interval(fetchStatistics, 30000);
+        $scope.$on('$destroy', function() {
+            if (angular.isDefined(fetchPromise)) {
+                $interval.cancel(fetchPromise);
+                fetchPromise = undefined;
+            }
+        });
     });
 })();
