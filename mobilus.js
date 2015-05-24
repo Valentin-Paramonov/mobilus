37f9980f4f08ae40b12c8a2d8e1c7dc334c0e852
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-05-24 22:22:31 +0300

    Add 404 template

--- mobilus.js
+++ mobilus.js
@@ -1,20 +1,28 @@
-const
-    HOME = __dirname,
-    PORT = 4848;
+const WEB_DIR = __dirname + '/web';
+const PARTIALS_DIR = __dirname + '/partials';
+const PORT = 4848;
 
-var mobilus = require('express')(),
-    bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
+var jadeStatic = require('connect-jade-static');
 
+var mobilus = express();
 mobilus.locals.pretty = true;
-mobilus.use(bodyParser.json());
 mobilus.set('view engine', 'jade');
-mobilus.set('views', HOME + '/views');
+mobilus.set('views', PARTIALS_DIR);
+
+mobilus.use(bodyParser.json());
+mobilus.use(express.static(WEB_DIR));
+mobilus.use(jadeStatic({
+    baseDir: PARTIALS_DIR, baseUrl: '/', jade: {
+        pretty: true
+    }
+}));
 
 mobilus.use('/test-data', require('./routes/test-data'));
 mobilus.use('/statistics', require('./routes/statistics'));
-
-mobilus.get('/log', function(request, response) {
-    response.render('log');
+mobilus.use(function(request, response) {
+    response.status(404).render('404');
 });
 
 mobilus.listen(PORT, function() {
