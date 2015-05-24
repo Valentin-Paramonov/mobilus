910f884bc2ab6731737c5bd4bb399192c3f2f744
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-05-24 23:11:41 +0300

    Implement simple statistics rendering

--- web/lib/mobile-stats.js
+++ web/lib/mobile-stats.js
@@ -1,3 +1,11 @@
 (function() {
     var app = angular.module('mobileStats', []);
+    app.controller('statsController', function($scope, $http) {
+        $http.get('http://52.28.111.52:4848/statistics')
+            .success(function(stats) {
+                $scope.stats = stats;
+            }).error(function(err) {
+                console.error(err);
+            });
+    });
 })();
